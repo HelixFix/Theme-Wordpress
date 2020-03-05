282ae699cc608191d7eb98114fc20415901117b3
HelixFix <jerome.rinner@gmail.com>
2020-03-05 15:27:58 +0100

    addThumbnailsOnPage

--- includes/section-archive.php
+++ includes/section-archive.php
@@ -1,20 +1,27 @@
 <?php if( have_posts() ): while( have_posts() ): the_post();?> <!--La boucle WordPress-->
 
 <div class="card mb-3"> <!--Ajoute une bordure et un margin bottom de 3-->
-    <div class="card-body">
+
+    <div class="card-body d-flex justify-content-center align-items-center">
         
         <?php if(has_post_thumbnail):?><!--Si possède une image-->
 
-        <img src="<?php the_post_thumbnail_url('blog-small');?>" alt="<?php the_title();?>" class="img-fluid mb-3 img-thumbnail"><!--Affiche l'image-->
+        <img src="<?php the_post_thumbnail_url('blog-small');?>" alt="<?php the_title();?>" class="img-fluid mb-3 img-thumbnail  mr-4"><!--Affiche l'image-->
 
-        <?php endif;?>   
-
-        <h3><?php the_title();?></h3>
+        <?php endif;?>
         
-        <?php the_excerpt();?>
+        <div class="blog-content">
 
-        <a href="<?php the_permalink();?>"class="btn btn-success">Lire plus</a> <!--Ajoute un lien-->
+            <h3><?php the_title();?></h3>
+            
+            <?php the_excerpt();?>
+
+            <a href="<?php the_permalink();?>"class="btn btn-success">Lire plus</a> <!--Ajoute un lien-->
+
+        </div>
+
     </div>
+
 </div>
 
 <?php endwhile; else: endif;?>
--- page.php
+++ page.php
@@ -6,6 +6,12 @@
 
         <h1><?php the_title();?></h1>
 
+        <?php if(has_post_thumbnail):?><!--Si possède une image-->
+
+        <img src="<?php the_post_thumbnail_url('blog-large');?>" alt="<?php the_title();?>" class="img-fluid mb-3 img-thumbnail"><!--Affiche l'image-->
+
+        <?php endif;?>   
+
         <?php get_template_part('includes/section', 'content');?> <!-- ceci==includes/section-content.php-->
     </div>
 </section> 
