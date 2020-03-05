e7ae771f3e5a121127a4f05c6249aab3629aa316
HelixFix <jerome.rinner@gmail.com>
2020-03-05 09:41:57 +0100

    addCategoryOnPage

--- archive.php
+++ archive.php
@@ -3,11 +3,11 @@
 <!--Template général-->
 <section class="page-wrap"><!--Ajoute de l'espace en haut et en bas-->
     <div class="container">
-
-        <!--<h1><?php the_title();?></h1>--> <!-- Amettre dans la boucle-->
+        <h1><?php echo single_cat_title();?></h1><!--Affiche le nom de la catégorie sur la page-->
+        <!--<h1><?php the_title();?></h1>--> <!-- A mettre dans la boucle-->
 
         <?php get_template_part('includes/section','archive');?> <!-- ceci==includes/section-content.php-->
-        
+
         <?php previous_posts_link();?> <!--Affiche lien suivant, ne pas oublier de definir le nombre d'article par page dans wordpress-->
         <?php next_posts_link();?>
     </div>
--- includes/section-blogcontent.php
+++ includes/section-blogcontent.php
@@ -20,18 +20,27 @@
 
         <?php
         $tags = get_the_tags();
-        foreach ($tags as $tag) : ?><!--Pour chaque contenu qui possède un tag-->
+        foreach ($tags as $tag) : ?><!--Boucle pour chaque contenu qui possède un tag-->
 
 
             <!--<?php echo get_tag_link($tag->term_id);?>--> <!--Récupérer le lien du tag-->
-            <a href="<?php echo get_tag_link($tag->term_id);?>">
-                <?php echo $tag->name;?>
+            <a href="<?php echo get_tag_link($tag->term_id);?>"class="badge badge-success"> <!--Lien clickable + mise en forme bouton tag-->
+                <?php echo $tag->name;?><!--Faire apparaitre nom du tag-->
             </a>
 
             
 
         <?php endforeach; ?>
 
-<?php endwhile;
-else : endif; ?>
-<!--Fin boucle WordPress-->
+
+        <?php
+        $categories = get_the_category();
+        foreach($categories as $cat) : ?><!--Boucle pour chaque contenu qui possède une catégorie-->
+                <a href="<?php echo get_category_link($cat->term_id);?>"> <!--Lien clickable + mise en forme bouton catégorie-->
+                <?php echo $cat->name;?><!--Faire apparaitre nom de la catégorie-->
+                </a>
+        <?php endforeach; ?>
+
+        
+
+<?php endwhile;else : endif; ?><!--Fin boucle WordPress-->
