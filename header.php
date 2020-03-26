29ef47c4750296eefa269777ac3e0c3be2a7e6cc
HelixFix <jerome.rinner@gmail.com>
2020-03-26 11:49:11 +0100

    menuGrid

--- header.php
+++ header.php
@@ -18,8 +18,10 @@
 
     <header>
 
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+    
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+            <a class="navbar-brand col-2" href="#"><?php bloginfo('name') ?></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                
                 <span class="navbar-toggler-icon"></span>
