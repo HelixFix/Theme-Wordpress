4edac1f729e11940f6e98bab934b95957293cae8
HelixFix <jerome.rinner@gmail.com>
2020-03-26 15:53:23 +0100

    searchFonctional

--- searchform.php
+++ searchform.php
@@ -9,7 +9,7 @@
 
 </form>  -->
 
-<form class="form-inline my-2 my-lg-0" action="<?php esc_url(home_url('/')) ?>">
+<form class="form-inline my-2 my-lg-0" action="https://jeromer401.promo-36.codeur.online/Wordpress/">
 
 	<input class="form-control mr-sm-2" name="s" type="search" placeholder="Recherche" aria-label="Search" value="<?php get_search_query() ?>">
 
