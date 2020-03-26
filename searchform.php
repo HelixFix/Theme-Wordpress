d38ff23cb87fb6aef7bd2817be5c3f2f312ef1bf
HelixFix <jerome.rinner@gmail.com>
2020-03-26 15:55:50 +0100

    changeSearchAction

--- searchform.php
+++ searchform.php
@@ -9,7 +9,7 @@
 
 </form>  -->
 
-<form class="form-inline my-2 my-lg-0" action="https://jeromer401.promo-36.codeur.online/Wordpress/">
+<form class="form-inline my-2 my-lg-0" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 
 	<input class="form-control mr-sm-2" name="s" type="search" placeholder="Recherche" aria-label="Search" value="<?php get_search_query() ?>">
 
